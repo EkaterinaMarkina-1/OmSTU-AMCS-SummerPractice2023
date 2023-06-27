a353b86fbfe0d452b33198f1cbcf43abf0db7818
Ekaterina <katya.05.2004.km@gmail.com>
2023-06-27 18:45:52 +0000

    adding a file

--- unit-testing-using-dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ unit-testing-using-dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -1,25 +1,53 @@
 using Xunit;
-using Prime.Services;
+using SquareEquationLib;
 
-namespace Prime.UnitTests.Services
+namespace PrimeService_Tests
 {
-    public class PrimeService_IsPrimeShould
+    public class PrimeService_Tests
     {
-        private readonly PrimeService _primeService;
 
-        public PrimeService_IsPrimeShould()
-        {
-            _primeService = new PrimeService();
-        }
-        [Theory]
-        [InlineData(-1)]
-        [InlineData(0)]
-        [InlineData(1)]
-        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
-        {
-            var result = _primeService.IsPrime(value);
+    [Theory]
+    [InlineData(double.NaN,1,1)]
+    [InlineData(1,double.NaN,1)]
+    [InlineData(1,1,double.NaN)]
 
-            Assert.False(result, $"{value} should not be prime");
-        }
+    [InlineData(double.PositiveInfinity,1,1)]
+    [InlineData(1,double.PositiveInfinity,1)]
+    [InlineData(1,1,double.PositiveInfinity)]
+
+    [InlineData(double.NegativeInfinity,1,1)]
+    [InlineData(1,double.NegativeInfinity,1)]
+    [InlineData(1,1,double.NegativeInfinity)]
+    public void testValidArgument(double a,double b, double c)
+    {
+        Assert.Throws<ArgumentException>(() => SquareEquation.Solve(a, b, c));
     }
+
+    [Theory]
+        [InlineData(1, 6, 9, new double[]{-3})]
+        [InlineData(10, 2, 10, new double[] {})]
+        [InlineData(1,2,-3, new double[] {1, -3})]
+        [InlineData(1, 0, 1, new double[]{})]
+        [InlineData(1, 4, 5, new double[]{})]
+
+        public void ValidFindRoot(double a, double b, double c, double[] expected)
+        {
+            
+        double[] actual = SquareEquation.Solve(a, b, c);
+
+
+            Array.Sort(actual);
+            Array.Sort(expected);
+
+            if (expected.Length != actual.Length)
+            {
+                Assert.Fail("Amount of roots does not match");
+            }
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i], actual[i], 5);
+            }
+        }
+}
 }
