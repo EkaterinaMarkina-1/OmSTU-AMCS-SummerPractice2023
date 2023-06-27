6b0deb172b8db4c65f3f37a30b6db77d6eded12f
Ekaterina <katya.05.2004.km@gmail.com>
2023-06-27 18:09:27 +0000

    adding a file

--- unit-testing-using-dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ unit-testing-using-dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -5,12 +5,21 @@ namespace Prime.UnitTests.Services
 {
     public class PrimeService_IsPrimeShould
     {
-        [Fact]
-        public void IsPrime_InputIs1_ReturnFalse()
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
         {
-            var primeService = new PrimeService();
-            bool result = primeService.IsPrime(1);
-            Assert.False(result, "1 should not be prime");
+            _primeService = new PrimeService();
+        }
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
         }
     }
 }
