a0b36549711c69ac542bd274109fceff9555f127
Ekaterina <katya.05.2004.km@gmail.com>
2023-06-29 19:43:50 +0000

    tests

--- Tests/PrimeService.Tests/PrimeService.Tests.cs
+++ Tests/PrimeService.Tests/PrimeService.Tests.cs
@@ -11,24 +11,23 @@ namespace SquareEquation_Test
     [InlineData(1,double.NaN,1)]
     [InlineData(1,1,double.NaN)]
 
-    [InlineData(double.PositiveInfinity,1,1)]
-    [InlineData(1,double.PositiveInfinity,1)]
-    [InlineData(1,1,double.PositiveInfinity)]
+    [InlineData(double.PositiveInfinity, 2, 3)]
+    [InlineData(2, double.PositiveInfinity, 3)]
+    [InlineData(2, 3, double.PositiveInfinity)]
 
-    [InlineData(double.NegativeInfinity,1,1)]
-    [InlineData(1,double.NegativeInfinity,1)]
-    [InlineData(1,1,double.NegativeInfinity)]
+     [InlineData(double.NegativeInfinity, 2, 3)]
+     [InlineData(2, double.NegativeInfinity, 3)]
+     [InlineData(2, 3, double.NegativeInfinity)]
     public void testValidArgument(double a,double b, double c)
     {
         Assert.Throws<ArgumentException>(() => SquareEquation.Solve(a, b, c));
     }
 
     [Theory]
-        [InlineData(1, 6, 9, new double[]{-3})]
-        [InlineData(10, 2, 10, new double[] {})]
-        [InlineData(1,2,-3, new double[] {1, -3})]
-        [InlineData(1, 0, 1, new double[]{})]
-        [InlineData(1, 4, 5, new double[]{})]
+        [InlineData(1, -3, 2, new double[] { 1, 2 })]
+        [InlineData(2, -7, 3, new double[] { 1, 1.5 })]
+        [InlineData(1, -2, 1, new double[] { 1 })]
+        [InlineData(1, 0, -1, new double[] { 1, -1 })]
 
         public void ValidFindRoot(double a, double b, double c, double[] expected)
         {
