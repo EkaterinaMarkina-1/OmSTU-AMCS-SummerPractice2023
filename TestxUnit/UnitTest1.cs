99cb881ffcd9c604e6c6208228d1c5ac9c3bde7b
Ekaterina <katya.05.2004.km@gmail.com>
2023-07-01 21:51:29 +0600

    test

--- TestxUnit/UnitTest1.cs
+++ TestxUnit/UnitTest1.cs
@@ -1,10 +1,58 @@
-namespace TestxUnit;
+using Xunit;
 
-public class UnitTest1
+namespace TestxUnit
 {
-    [Fact]
-    public void Test1()
+    public class SpaceShipTests
     {
+        [Fact]
+        public void SpaceShipMovesInStraightLine()
+        {
+            // Arrange
+            var spaceShip = new SpaceShip();
+            spaceShip.SetPosition(12, 5);
+            spaceShip.SetVelocity(-5, 3);
+            // Act
+            spaceShip.Move();
+            // Assert
+            Assert.Equal(7, spaceShip.GetPosX());
+            Assert.Equal(8, spaceShip.GetPosY());
+        }
 
+        [Fact]
+        public void CannotMoveIfPositionNotDefined()
+        {
+            // Arrange
+            var spaceShip = new SpaceShip();
+            spaceShip.SetVelocity(-5, 3);
+
+            // Act & Assert
+            Assert.Throws<System.Exception>(() => spaceShip.Move());
+        }
+
+        [Fact]
+        public void CannotMoveIfVelocityNotDefined()
+        {
+            // Arrange
+            var spaceShip = new SpaceShip();
+            spaceShip.SetPosition(12, 5);
+
+            // Act & Assert
+            Assert.Throws<System.Exception>(() => spaceShip.Move());
+        }
+
+        [Fact]
+        public void CannotMoveIfPositionCannotBeChanged()
+        {
+            // Arrange
+            var spaceShip = new SpaceShip();
+            spaceShip.SetPosition(12, 5);
+            spaceShip.SetVelocity(-5, 3);
+            // Assuming there is some logic in SpaceShip class to make position unchangeable
+            spaceShip.SetPositionUnchangeable(true);
+
+            // Act & Assert
+            Assert.Throws<System.Exception>(() => spaceShip.Move());
+        }
     }
 }
+
