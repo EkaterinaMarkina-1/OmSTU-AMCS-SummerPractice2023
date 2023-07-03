154d57e845aa160ad13a7d9e9ad1da39837e2489
Ekaterina <katya.05.2004.km@gmail.com>
2023-07-04 01:20:52 +0600

    bdd_tests

--- BDD.Tests/BDD.Tests.cs
+++ BDD.Tests/BDD.Tests.cs
@@ -8,40 +8,56 @@ namespace SquareEquationBDD
     [Binding]
     public class TestBDD
     {
-        private double a;
-        private double b;
-        private double c;
+        private double a, b, c;
         private double[] roots;
+        private Exception actualException;
 
-        [Given(@"квадратное уравнение с коэффициентами a = (.+), b = (.+), c = (.+)")]
-        public void КвадратноеУравнение(double a, double b, double c)
+        [Given(@"^Квадратное уравнение с коэффициентами \((.*), (.*), (.*)\)")]
+        public void КвадратноеУравнение(string p0, string p1, string p2)
         {
-            this.a = a;
-            this.b = b;
-            this.c = c;
+            a = double.Parse(p0);
+            b = double.Parse(p1);
+            c = double.Parse(p2);
         }
 
-        [When(@"находим корни квадратного уравнения")]
+        [When(@"^вычисляются корни квадратного уравнения")]
         public void НахождениеКорней()
         {
-            roots = SquareEquation.Solve(a, b, c);
+            try
+            {
+                roots = SquareEquation.Solve(a, b, c);
+            }
+            catch (Exception e)
+            {
+                actualException = e;
+            }
         }
 
-        [Then(@"квадратное уравнение имеет два корня, кратности один")]
-        public void ТоКвадратноеУравнениеИмеетДваКорняКратностиОдин()
+        [Then(@"квадратное уравнение имеет два корня \((.*), (.*)\) кратности один")]
+        public void ТоКвадратноеУравнениеИмеетДваКорняКратностиОдин(double expectedRoot1, double expectedRoot2)
         {
-            Assert.Equal(2, roots.Length);
-            Assert.NotEqual(roots[0], roots[1]);
+            double[] expectedRoots = new double[] { expectedRoot1, expectedRoot2 };
+            Array.Sort(expectedRoots);
+            Array.Sort(roots);
+            for (int i = 0; i < expectedRoots.Length; i++)
+            {
+                Assert.Equal(expectedRoots[i], roots[i], 6);
+            }
         }
 
-        [Then(@"квадратное уравнение имеет один корень, кратности два")]
-        public void ТоКвадратноеУравнениеИмеетОдинКореньКратностиДва()
+        [Then(@"квадратное уравнение имеет один корень (.*) кратности два")]
+        public void ТоКвадратноеУравнениеИмеетОдинКореньКратностиДва(double expectedRoot)
         {
-            Assert.Equal(2, roots.Length);
-            Assert.Equal(roots[0], roots[1]);
+            double[] expectedRoots = new double[] { expectedRoot };
+            Array.Sort(expectedRoots);
+            Array.Sort(roots);
+            for (int i = 0; i < expectedRoots.Length; i++)
+            {
+                Assert.Equal(expectedRoots[i], roots[i], 6);
+            }
         }
 
-        [Then(@"квадратное уравнение не имеет корней")]
+        [Then(@"множество корней квадратного уравнения пустое")]
         public void ТоКвадратноеУравнениеНеИмеетКорней()
         {
             Assert.Empty(roots);
@@ -50,7 +66,7 @@ namespace SquareEquationBDD
         [Then(@"выбрасывается исключение ArgumentException")]
         public void ТоВыбрасываетсяИсключениеArgumentException()
         {
-            Assert.Throws<System.ArgumentException>(() => SquareEquation.Solve(a, b, c));
+            Assert.Throws<System.ArgumentException>(() => throw actualException);
         }
     }
 }
