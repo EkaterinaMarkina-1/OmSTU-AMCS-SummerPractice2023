392bd23508d959cf68b633d2d4aae581f3c0bd38
Ekaterina <katya.05.2004.km@gmail.com>
2023-07-05 15:36:36 +0600

    space_ship

--- SpaceBattle/SpaceBattle.cs
+++ SpaceBattle/SpaceBattle.cs
@@ -1,8 +1,11 @@
+
 namespace SpaceWarLib
 {
     public class SpaceMove
     {
         public static double[] Movement(double[] SpaceShipCoordinates,double[] SpaceShipWay, bool opportunityMove ){
+
+        
 
         if (opportunityMove == false) {
             throw new System.Exception();
@@ -23,4 +26,31 @@ namespace SpaceWarLib
         }
 
     }
+
+    public class ShipFuel{
+        public static double fuelCalculate(double fuelBalance ,double fuelNeed){
+            if(fuelNeed>fuelBalance){
+                throw new System.Exception();
+            }
+            return fuelBalance-fuelNeed;
+        }
+    }
+
+    public class ShipRotate{
+        public static double Rotation (double degX,double instantSpeed,bool rotate){
+            foreach(double x in new double[]{degX, instantSpeed})
+        {
+            
+            if (double.IsNaN(x) | double.IsInfinity(x)) {
+                throw new System.Exception();
+            }
+        }
+        if (rotate == false) {
+            throw new System.Exception();
+        }
+
+        return degX + instantSpeed;
+        }
+    }
+
 }
